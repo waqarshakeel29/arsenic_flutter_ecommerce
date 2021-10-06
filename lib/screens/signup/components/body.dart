43628def0dd115adde9d0d151bc934a823eb99a9
waqarshakeel29 <waqarshakeel29@gmail.com>
2021-10-07 01:48:47 +0500

    login - sign up -> screens added

---
+++ lib/screens/signup/components/body.dart
@@ -0,0 +1,80 @@
+import 'package:arsenic_flutter_ecommerce/components/already_have_an_account_acheck.dart';
+import 'package:arsenic_flutter_ecommerce/components/rounded_button.dart';
+import 'package:arsenic_flutter_ecommerce/components/rounded_input_field.dart';
+import 'package:arsenic_flutter_ecommerce/components/rounded_password_field.dart';
+import 'package:arsenic_flutter_ecommerce/screens/login/login_screen.dart';
+import 'package:arsenic_flutter_ecommerce/screens/signup/components/social_icon.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/svg.dart';
+
+import 'background.dart';
+import 'or_divider.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "SIGNUP",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            OrDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SocalIcon(
+                  iconSrc: "assets/icons/facebook.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/twitter.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/google-plus.svg",
+                  press: () {},
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
