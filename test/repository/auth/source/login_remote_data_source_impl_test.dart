3530b938b0ee6b6fcf0810d69c3b23e65184c2f3
waqarshakeel29 <waqarshakeel29@gmail.com>
2021-10-10 23:04:57 +0500

    test refector

--- test/repository/auth/source/login_remote_data_source_impl_test.dart
+++ test/repository/auth/source/login_remote_data_source_impl_test.dart
@@ -12,23 +12,8 @@ import 'login_remote_data_source_impl_test.mocks.dart';
 
 @GenerateMocks([
   FirebaseAuth,
-  // User,
-  // UserCredential,
-  // AuthResponse,
-  // ServerError,
-  // AppUser,
 ])
 void main() {
-  // void setUpMockHttpClientSuccess200() {
-  //   when(mockFirebaseAuth.get(any, headers: anyNamed('headers')))
-  //       .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
-  // }
-
-  // void setUpMockHttpClientFailure404() {
-  //   when(mockHttpClient.get(any, headers: anyNamed('headers')))
-  //       .thenAnswer((_) async => http.Response('Something went wrong', 404));
-  // }
-
   group('sign in', () {
     final userName = "i170249@nu.edu.pk";
     final password = "nomi";
@@ -43,11 +28,6 @@ void main() {
         dataSource =
             LoginRemoteDataSourceImpl(firebaseAuthInstance: mockFirebaseAuth);
 
-        // when(mockFirebaseAuth.currentUser).thenAnswer((_) => MockUser());
-        // when(mockFirebaseAuth.signInWithEmailAndPassword(
-        //         email: userName, password: password))
-        //     .thenAnswer((_) async => MockUserCredential());
-
         when(dataSource.loginWithUserNamePassword(userName, password))
             .thenAnswer((_) async =>
                     // Future<Either<MockServerError, MockAuthResponse>>.value(
@@ -58,98 +38,11 @@ void main() {
                 // )
                 );
 
-        // arrange
-        // setUpMockHttpClientSuccess200();
-        // act
-
-        // assert
-        // verify(
-        //   await mockFirebaseAuth.signInWithEmailAndPassword(
-        //       email: userName, password: password),
-        // );
         expect(
             await mockFirebaseAuth.signInWithEmailAndPassword(
                 email: userName, password: password),
             isA<UserCredential>());
-
-        // expect(
-        //     (dataSource.loginWithUserNamePassword(userName, password))
-        //     // .fold((loginError) => LoginError("Unable to Login"),
-        //     //     (authResponse) => authResponse.appUser)
-        //     ,
-        //     isA<LoginError>());
       },
     );
-
-    // test(
-    //   'should return NumberTrivia when the response code is 200 (success)',
-    //   () async {
-    //     // arrange
-    //     setUpMockHttpClientSuccess200();
-    //     // act
-    //     final result = await dataSource.getConcreteNumberTrivia(tNumber);
-    //     // assert
-    //     expect(result, equals(tNumberTriviaModel));
-    //   },
-    // );
-
-    // test(
-    //   'should throw a ServerException when the response code is 404 or other',
-    //   () async {
-    //     // arrange
-    //     setUpMockHttpClientFailure404();
-    //     // act
-    //     final call = dataSource.getConcreteNumberTrivia;
-    //     // assert
-    //     expect(() => call(tNumber), throwsA(TypeMatcher<ServerException>()));
-    //   },
-    // );
   });
-
-  // group('getRandomNumberTrivia', () {
-  //   final tNumberTriviaModel =
-  //       NumberTriviaModel.fromJson(json.decode(fixture('trivia.json')));
-
-  //   test(
-  //     '''should perform a GET request on a URL with number
-  //      being the endpoint and with application/json header''',
-  //     () async {
-  //       // arrange
-  //       setUpMockHttpClientSuccess200();
-  //       // act
-  //       dataSource.getRandomNumberTrivia();
-  //       // assert
-  //       verify(mockHttpClient.get(
-  //         'http://numbersapi.com/random',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //       ));
-  //     },
-  //   );
-
-  //   test(
-  //     'should return NumberTrivia when the response code is 200 (success)',
-  //     () async {
-  //       // arrange
-  //       setUpMockHttpClientSuccess200();
-  //       // act
-  //       final result = await dataSource.getRandomNumberTrivia();
-  //       // assert
-  //       expect(result, equals(tNumberTriviaModel));
-  //     },
-  //   );
-
-  //   test(
-  //     'should throw a ServerException when the response code is 404 or other',
-  //     () async {
-  //       // arrange
-  //       setUpMockHttpClientFailure404();
-  //       // act
-  //       final call = dataSource.getRandomNumberTrivia;
-  //       // assert
-  //       expect(() => call(), throwsA(TypeMatcher<ServerException>()));
-  //     },
-  //   );
-  // });
 }
